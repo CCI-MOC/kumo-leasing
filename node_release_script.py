095dbfd3d3bc1b34e8e69e823bcfec05439d787a
amin mosayyebzadeh <mosayyeb@bu.edu>
2018-02-01 15:43:34 -0500

    Removing the conflict

--- node_release_script.py
+++ node_release_script.py
@@ -3,6 +3,8 @@
 This script will free nodes from a project after
 a specific amount of time.
 
+It should run on the BMI client machine.
+
 You can find more information including
 config file and status file format in
 README file.
